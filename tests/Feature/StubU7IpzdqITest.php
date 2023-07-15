0f418f6d5daf882ced06f06d590e21487ae7fca1
jradtilbrook <jarryd@tilbrooktech.com>
2023-07-15 15:27:52 +0800

    Add 500 tests

---
+++ tests/Feature/StubU7IpzdqITest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Lottery;
+use Illuminate\Support\Sleep;
+use Tests\TestCase;
+
+class StubU7IpzdqITest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_example_2(): void
+    {
+        Lottery::odds(1, 4)
+            ->winner(fn () => usleep(200000))
+            ->loser(fn () => usleep(30000))
+            ->choose();
+
+        $this->assertTrue(true);
+    }
+}
